0d6ab7212c6d3c71902c1f00de5cdb8cc5c870fa
Miraslau <miraslausharkouski@gmail.com>
2025-06-30 19:14:10 +0300

    add base in vite

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,14 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import Components from "unplugin-react-components/vite";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
+
     Components({
       dts: true,
       include: [/\.tsx?$/, /\.jsx?$/],
     }),
   ],
+  base: "/my-table-app",
 });
